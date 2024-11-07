10bcc01b27767f56552ddd5f9904a1307282681a
Ned Palacios <npdoesmc@gmail.com>
2024-11-07 11:10:20 +0800

    fix: export jsf and json schema types too

--- lib/formuler.ts
+++ lib/formuler.ts
@@ -1,4 +1,6 @@
 export * from "./components/FormRenderer";
 export * from "./controller_context";
 export * from "./types/form";
+export * from "./types/json_schema";
+export * from "./types/json_schema_form";
 export * from "./renderers";
